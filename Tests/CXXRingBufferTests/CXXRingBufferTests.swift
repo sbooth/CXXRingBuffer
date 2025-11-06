3387c8cab8a2b48f38864573eeae51ed7aa24bf9
Stephen F. Booth <me@sbooth.org>
2025-11-06 12:33:36 -0600

    Add basic read/write test

--- Tests/CXXRingBufferTests/CXXRingBufferTests.swift
+++ Tests/CXXRingBufferTests/CXXRingBufferTests.swift
@@ -27,9 +27,29 @@ import Foundation
 		#expect(rb.Allocate(2) == true)
 		#expect(rb.Allocate(0x7FFFFFFF) == true)
 
-		rb.Allocate(1024)
+		#expect(rb.Allocate(1024) == true)
 		#expect(rb.CapacityBytes() >= 1024)
 		#expect(rb.BytesAvailableToRead() == 0)
 		#expect(rb.BytesAvailableToWrite() >= 1024)
+	}
+
+	@Test func basic() {
+		var rb = CXXRingBuffer.SFB.RingBuffer()
+
+		#expect(rb.Allocate(128) == true)
+
+		let written = Data(stride(from: 0, through: 15, by: 1))
+		written.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
+			#expect(rb.Write(ptr.baseAddress!, UInt32(written.count)) == 16)
+		}
+		#expect(rb.BytesAvailableToRead() == written.count)
+
+		var read = Data(count: written.count)
+		read.withUnsafeMutableBytes { (ptr: UnsafeMutableRawBufferPointer) in
+			#expect(rb.Read(ptr.baseAddress!, 16) == 16)
+		}
+
+		#expect(read == written)
+		#expect(rb.BytesAvailableToRead() == 0)
 	}
 }
