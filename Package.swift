85681ee616701bc243e8840afd480ff3bcba148f
Stephen Booth <me@sbooth.org>
2025-11-16 15:48:17 -0600

    Move header files to `CXXRingBuffer` directory (#14)
    Also renames the enclosing namespce to `CXXRingBuffer`

--- Package.swift
+++ Package.swift
@@ -18,7 +18,10 @@ let package = Package(
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
-			name: "CXXRingBuffer"
+			name: "CXXRingBuffer",
+			cSettings: [
+				.headerSearchPath("include/CXXRingBuffer"),
+			]
 		),
 		.testTarget(
 			name: "CXXRingBufferTests",
