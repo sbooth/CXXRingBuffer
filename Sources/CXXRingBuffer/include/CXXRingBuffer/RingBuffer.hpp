f3607155a266f8c3b5aaa0cbe8ddb3a854dfa92a
Stephen Booth <me@sbooth.org>
2026-01-02 11:29:57 -0600

    Update to follow reuse.software suggestions

--- Sources/CXXRingBuffer/include/CXXRingBuffer/RingBuffer.hpp
+++ Sources/CXXRingBuffer/include/CXXRingBuffer/RingBuffer.hpp
@@ -1,6 +1,8 @@
 //
-// SPDX-FileCopyrightText: 2014-2026 Stephen F. Booth <https://github.com/sbooth/CXXRingBuffer>
+// SPDX-FileCopyrightText: 2014 Stephen F. Booth <contact@sbooth.dev>
 // SPDX-License-Identifier: MIT
+//
+// Part of https://github.com/sbooth/CXXRingBuffer
 //
 
 #pragma once
