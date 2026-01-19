1b18992bc7d82a26aed61c93a4c45d0900bdf4a9
Stephen Booth <me@sbooth.org>
2026-01-18 19:53:00 -0600

    Tabs to spaces

--- Package.swift
+++ Package.swift
@@ -9,34 +9,34 @@
 import PackageDescription
 
 let package = Package(
-	name: "CXXRingBuffer",
-	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
-		.library(
-			name: "CXXRingBuffer",
-			targets: [
-				"CXXRingBuffer",
-			]
-		),
-	],
-	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
-		.target(
-			name: "CXXRingBuffer",
-			cSettings: [
-				.headerSearchPath("include/CXXRingBuffer"),
-			]
-		),
-		.testTarget(
-			name: "CXXRingBufferTests",
-			dependencies: [
-				"CXXRingBuffer",
-			],
-			swiftSettings: [
-				.interoperabilityMode(.Cxx),
-			]
-		),
-	],
-	cxxLanguageStandard: .cxx20
+    name: "CXXRingBuffer",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CXXRingBuffer",
+            targets: [
+                "CXXRingBuffer",
+            ]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CXXRingBuffer",
+            cSettings: [
+                .headerSearchPath("include/CXXRingBuffer"),
+            ]
+        ),
+        .testTarget(
+            name: "CXXRingBufferTests",
+            dependencies: [
+                "CXXRingBuffer",
+            ],
+            swiftSettings: [
+                .interoperabilityMode(.Cxx),
+            ]
+        ),
+    ],
+    cxxLanguageStandard: .cxx20
 )
